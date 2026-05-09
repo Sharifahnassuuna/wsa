78b6b2e11eeccfcf7fc296f4f3b8ec33aa8fa4f5
Sharifahnassuuna <nassuunasharifah2@gmail.com>
2026-05-09 12:37:43 +0300

    Update welcome message in Kirabo_babrah's profile

--- conquerers/members/kirabo_babrah/index.php
+++ conquerers/members/kirabo_babrah/index.php
@@ -11,7 +11,7 @@
 </head>
 <body>
     <div class="profile">
-        <h1>Welcome to Kirabo_babrah's subdomain sampling</h1>
+        <h1>Welcome to Kirabo_babrah's subdomain</h1>
         
         <?php
         $stmt = $pdo->prepare("SELECT * FROM members WHERE name = ?");
